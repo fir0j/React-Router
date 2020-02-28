6abda608d47573c4b0fd9b7e47333d3e003990fc
firoj <firoj@localhost.localdomain>
2020-02-28 18:34:59 +0530

    added authentication feature

--- src/App.js
+++ src/App.js
@@ -12,14 +12,23 @@ import {
 } from 'react-router-dom';
 
 // Home Page
-const Homepage = () => (
+const HomePage = () => (
 	<Fragment>
 		<h1>Home page</h1>
 	</Fragment>
 );
 
+const Welcome = () => {
+	const { name } = useParams();
+	return (
+		<Fragment>
+			<p> About {name}</p>
+		</Fragment>
+	);
+};
+
 // About Page
-const About = () => {
+const AboutPage = () => {
 	// getting url and path dynamically instead of manually typing in <Link/> component
 	const { url, path } = useRouteMatch();
 
@@ -54,17 +63,8 @@ const About = () => {
 	);
 };
 
-const Welcome = () => {
-	const { name } = useParams();
-	return (
-		<Fragment>
-			<p>Welcome {name}</p>
-		</Fragment>
-	);
-};
-
 // Contact Page
-const Contact = () => {
+const ContactPage = () => {
 	return (
 		<Fragment>
 			<h1>Contact Page</h1>
@@ -72,7 +72,95 @@ const Contact = () => {
 	);
 };
 
-// Link is similar to <a> tag and to is similar to href attribute in html.
+function ProfilePage() {
+	let history = useHistory();
+
+	return fakeAuth.isAuthenticated ? (
+		<Fragment>
+			<p>Welcome to your ProfilePage !</p>
+			<button
+				className="border"
+				onClick={() => {
+					fakeAuth.signout(() => history.push('/'));
+				}}
+			>
+				Sign out
+			</button>
+		</Fragment>
+	) : (
+		<p>You are not logged in.</p>
+	);
+}
+
+// Redirect vs push vs history in react-router-dom
+
+const LoginPage = () => {
+	let history = useHistory();
+	let location = useLocation();
+
+	let { from } = location.state || { from: { pathname: '/' } };
+	let login = () => {
+		fakeAuth.authenticate(() => {
+			history.replace(from);
+		});
+	};
+
+	return (
+		<div>
+			<p>You must log in to view the page at {from.pathname}</p>
+			<button className="border" onClick={login}>
+				Log in
+			</button>
+		</div>
+	);
+};
+
+const fakeAuth = {
+	isAuthenticated: false,
+
+	authenticate(callback) {
+		fakeAuth.isAuthenticated = true;
+		callback();
+	},
+
+	signout(callback) {
+		fakeAuth.isAuthenticated = false;
+		callback();
+	}
+};
+
+// Protecting Routes using react-router-dom
+
+function PrivateRoute({ children, ...rest }) {
+	// children contains all the child component of </Route> as props.
+	// rest is the props passed to this component
+	const { location, ...others } = rest;
+	return (
+		<Route
+			{...rest}
+			render={() =>
+				fakeAuth.isAuthenticated ? (
+					children
+				) : (
+					<Redirect
+						to={{
+							pathname: '/login',
+							state: { from: location }
+						}}
+					/>
+				)}
+		/>
+	);
+}
+
+// <BrowserRouter/> component needs monitors the route change
+// It is better to wrap all Routes with <Switch/> Component to ensure rendering of only one path (that which matches first) at a time in case more than one match found.
+// exact means full path need to be matched in order for the components to render
+// if no path attribute is set on Route compoenent, it is default for rendering no path is matched( useful in scenerio like page not found)
+// using children attribute, all the components' params can accessed into the child component
+
+// location.pathname represents the root-relative url.
+// match.url represents the matched portion of the URL, so maybe a portion of location.pathname in case complete url is not matched.
 
 const Navbar = () => {
 	return (
@@ -89,9 +177,7 @@ const Navbar = () => {
 						<Link to="/contact">Contact</Link>
 					</li>
 					<li>
-						<Link to="/signin">
-							<button>SignIn</button>
-						</Link>
+						<Link to="/profile">Profile</Link>
 					</li>
 				</ul>
 			</nav>
@@ -99,11 +185,12 @@ const Navbar = () => {
 	);
 };
 
-// <BrowserRouter/> component needs monitors the route change
-// It is better to wrap all Routes with <Switch/> Component to ensure rendering of only one path (that which matches first) at a time in case more than one match found.
-// exact means full path need to be matched in order for the components to render
-// if no path attribute is set on Route compoenent, it is default for rendering no path is matched( useful in scenerio like page not found)
-// using children attribute, all the components' params cab accessed into the child component
+// <Route/> component expects the path string from the <Link/> Component called pathname which is always relative to /.  and tries to the path string specified to itself.
+// The portion of the pathname which matched with the path callled url in react-router-dom so it may not start from / always. Hence in case whole path name is matched, pathname and url looks same.
+// Link is similar to <a> tag and to is similar to href attribute in html.
+// to attribute takes path/pathname as string and passes to all the Route components
+// whenever is pathname is matched with path, an object is passed as props to the child component which is inside the <Route/>
+// the looks like {path: " same PATH attribute from <Route/>", location:" PATHNAME, state:{from: ""} ", computedMatch:" URL"}.
 
 export default function TurtorialApp() {
 	const [ isAuthenticated, setIsAuthenticated ] = useState(true);
@@ -114,25 +201,21 @@ export default function TurtorialApp() {
 			<main>
 				<Switch>
 					<Route exact path="/">
-						<Homepage />
+						<HomePage />
 					</Route>
-					{isAuthenticated ? (
-						<Fragment>
-							<Switch>
-								<Route path="/about">
-									<About />
-								</Route>
-								<Route path="/contact">
-									<Contact />
-								</Route>
-								<Route render={() => <h1>404: page not found</h1>} />
-							</Switch>
-						</Fragment>
-					) : (
-						<Fragment>
-							<Redirect to="/" />
-						</Fragment>
-					)}
+					<Route path="/about">
+						<AboutPage />
+					</Route>
+					<Route path="/contact">
+						<ContactPage />
+					</Route>
+					<PrivateRoute path="/profile">
+						<ProfilePage />
+					</PrivateRoute>
+					<Route path="/login">
+						<LoginPage />
+					</Route>
+					<Route render={() => <h1>404: page not found</h1>} />
 				</Switch>
 			</main>
 		</Router>
